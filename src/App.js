d0f7a7eb3b5f38a3e0cc5c7b9a1e28cf20ee5546
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-03-12 18:38:09 +0000

    Deleted some unnecessary files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Weather App 
+      <footer>
+        Project created by Maryan Ali.
+      <a href="https://github.com/Maryanali/react-web-app" target="_blank"> {""}Open sourced on Github</a>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
